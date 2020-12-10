d5bbd55b5f566d03441fc832323b39c295fe1841
Meghan Carlson <mcarlson552@outlook.com>
2020-12-10 11:47:48 -0600

    added part 2 and comments

--- train_chatbot.py
+++ train_chatbot.py
@@ -1,12 +1,31 @@
 import numpy as np                                  #for numpy
 from keras.models import Sequential                 #sequential model
 from keras.layers import Dense, Activation, Dropout #layers
-from keras.optimizers import SGD                    #
-import random
-import nltk
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-import json
-import pickle
+from keras.optimizers import SGD                    #optimization algorithm
+import random                                       #random module
+import nltk                                         #natural language toolkit
+from nltk.stem import WordNetLemmatizer             #Lemmatization
+lemmatizer = WordNetLemmatizer()                    #Lemmatization
+import json                                         #json module
+import pickle                                       #pickle module
 intents_file = open('intents.json').read()
 intents = json.loads(intents_file)
+
+
+#Part 2: Preprocessing the Data
+words = []
+classes = []
+documents = []
+ignore_letters = ['!', '?', ',', '.']
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        #tokenize each word
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        #add documents in the corpus
+        documents.append((word, intent['tag']))
+        # add to our classes list
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+print(documents)
